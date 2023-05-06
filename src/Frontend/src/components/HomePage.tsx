5d3379b21f88c39229853f74fafecccb615f3623
emoly1002 <110625232+emoly10022@users.noreply.github.com>
2023-05-06 16:52:33 -0300

    add navBar

---
+++ src/Frontend/src/components/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useState } from 'react';
+
+export const HomePage = () => {
+  return (
+    <div className="mt-12 h-full flex items-center justify-center flex-col">
+KIKAAA
+    </div>
+  )
+}
---
+++ src/Frontend/src/components/ProcessoSeletivo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useState } from 'react';
+
+
+export const ProcessoSeletivo = () => {
+  return (
+    <div className="mt-12 h-full flex items-center justify-center flex-col">
+      teste2
+    </div>
+  )
+}

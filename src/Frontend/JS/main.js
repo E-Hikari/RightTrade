c50ac50043cb994d5d2503fdbc1a87d625947946
E-Hikari <hikarik.nunes@gmail.com>
2023-05-07 01:51:02 -0300

    pag inicial

---
+++ src/Frontend/JS/main.js
@@ -0,0 +1,12 @@
+
+AOS.init({
+  // Configurações dos atributos 'data-aos'  Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // deslocamento em pixels da offset da imagem inicial
+  delay: 0, // delay entre os valores de 0 a 3000, values from 0 to 3000, com tempo de 50ms
+  duration: 900, // delay entre os valores de 0 a 3000, values from 0 to 3000, com tempo de 50ms
+  easing: 'ease', // animações AOS padrão
+  once: false, // Se a animação deveria apenas acontecer uma vez - quando rola a tela pra baixo
+  mirror: false, // Se a animação deveria acontecer quando o usuário passa por elas
+  anchorPlacement: 'top-bottom', // define qual posição em relação a página deve ativar a animação
+
+});

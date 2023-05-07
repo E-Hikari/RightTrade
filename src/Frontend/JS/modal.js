1702dc55e8a7788fd53697488dceba5167827e56
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2023-05-07 05:18:10 -0300

    outros alertas e links

--- src/Frontend/JS/modal.js
+++ src/Frontend/JS/modal.js
@@ -2,15 +2,30 @@ function sweetAlert() {
     Swal.fire(
         'Parabéns',
         'Inscrição realizada com sucesso!',
-        'success'
+        'success',
+        
       )
 }
 
 
 function uploadProject() {
-  Swal.fire(
-    'Upload efetuado com sucesso!',
-    '',
-    'success'
-  )
+  // pede o arquivo da pessoa :)
+
+  Swal.fire({
+    title: 'Tem certeza?',
+    text: "Não é possível alterar o updlaod!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Sim!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Upload completo!',
+        'Seu arquivo foi enviado ao solicitante, em até 7 dias ele retornará com uma resposta!',
+        'success'
+      )
+    }
+  })
 }
